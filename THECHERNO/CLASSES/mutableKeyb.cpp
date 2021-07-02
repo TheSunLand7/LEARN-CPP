1f0813820d09fab9a38ddfa0247ecba18fb70081
JaeZero <jos.ct101@gmail.com>
2021-07-02 00:18:47 -0500

    Last change

---
+++ THECHERNO/CLASSES/mutableKeyb.cpp
@@ -0,0 +1,16 @@
+//
+// Created by JaeZero on 2021.07.01 -- Thursday at 23:54:09
+//
+// --> In this file we discuss about Mutable keyboard.
+//
+// COMMENTS BELOW ARE IN SPANISH
+//
+#include <iostream>
+
+//MUTABLE: 
+
+int main() {
+	/* --Do something -- */
+
+	std::cin.get(); return 0;
+}
