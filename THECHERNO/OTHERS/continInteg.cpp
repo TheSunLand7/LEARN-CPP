46bfefc4a092d6071a59a066cb74b2ab99632319
JaeZero <jos.ct101@gmail.com>
2021-07-20 17:50:33 -0500

    Update

---
+++ THECHERNO/OTHERS/continInteg.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 夏禹(XiàYǔ) on 2021.07.20 -- Tuesday at 16:20:25
+//
+// --> In this file we discuss about CONTINUOUS INSTEGRATION.
+//
+// COMMENTS BELOW ARE IN SPANISH
+//
+#include <iostream>
+
+//CONTINUOUS INTEGRATION (CI): Es el proceso de integrar continuamente nuestro codigo durante su desarrollo.
+//En otras palabras, construir automatizacion y testeo. Podemos utilizar plataformas como Jenkins, TeamCity.
+
+int main()
+{
+
+    std::cin.get();
+    return 0;
+}
